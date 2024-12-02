fea2aa1e583e13ff237c5cfc4077dd1e588f46fc
Md. Navid Iqbal <144615765+mdnavidiqbal@users.noreply.github.com>
2024-12-02 15:13:54 +0600

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,16 +1,57 @@
 """
-#input() function 
-x=input("Enter a number : ")
-print(x)
-"""
-# type() function 
-# help() function 
-"""help(input)"""
+# 1.input() function we use it get input from the user 
+num = int(input("Enter any number : "))
+print("The number is : ",num)
+
 """
 
-# range() function use to print number without write again and again 
-for i in range(1,5):
-    print("i")
- 
- """
+"""
+# 2.type() function we use it to find out the type of the value 
+text = "Navid Iqbal"
+print(type(text))
+
+"""
+
+"""
+# 3.help() function we use it to see the documentation of a function 
+print(help(input))
+"""
+
+"""
+# 4.len() funtion we use it to the length of string or other data 
+
+text = "Navid"
+print(len(text))
+"""
+
+"""
+# 5.max() function we use it to find the maximum value of list 
+roll_list=[20,40,23,8,7]
+print(max(roll_list))
+"""
+
+"""
+# 5.min() function we use it to find the maximum value of lis
+roll_list=[20,40,23,8,7]
+print(min(roll_list))
+"""
+
+"""
+# 6.sorted() function we use to rearrange the data down to up 
+roll_list=[20,40,23,8,7]
+print(sorted(roll_list))
+"""
+
+"""
+# 7. reverse() function we use to reverse the data 
+roll_list =[1,2,3,4,5]
+print(reversed(roll_list))
+"""
+
+
+"""
+# 8.sum() function we use it calculate the sum of a list 
+roll_list =[1,2,3,4,5]
+print(sum(roll_list))
+"""
 
