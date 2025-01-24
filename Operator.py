6fbc07443641acf6c097f60078dc5f1dea5f9e09
Md. Navid Iqbal <144615765+mdnavidiqbal@users.noreply.github.com>
2025-01-24 19:44:13 +0600

    Update Operator.py

--- Operator.py
+++ Operator.py
@@ -1,21 +1,44 @@
-"""
-First of all Mathmetical operator like 1.+,2.-,3.*,4./,5.**(Squar/power),6.//(Integer Division)
-7.%(Modulas or Reminder),8.()Bracket
-"""
-"""
-#Modulas or Reminder 
-x=5
-y=2
-H=x%y
-print(H)
-# Integer Division
-x=5
-y=2
-H=x//y
-print(H)
-# Squar or Power 
-x=5
-y=3
-H=x**y
-print(H)
-"""
+# # Mathmetical Operatior 
+# # Here use string formatting and addition(+)
+# a = 10
+# b = 20
+# print(f"Sum of two num is : {a+b}")
+
+# # Subtraction (-)
+# print(f"{a-b}")
+
+# # Multiplication (*)
+# print(f"{a * b}")
+
+# # Division (/)
+# print(f"{a/b}")
+
+# # Modulus (%)
+# print(f"{a%b}")
+
+# # Exponential (**) 
+# x = 3
+# y = 2
+# print(f"{x ** y}")
+
+# # Flood division (//) 
+# sum1 = 15
+# sum2 = 2
+# print(f"{sum1 // sum2}")
+
+# Assignment operator 
+
+# a=10 
+# a += 10 # a = a + 10
+# print(a)
+
+# a -= 2 # a = a - 2
+# print(a)
+
+# Comparision Operator 
+# 1. == (equal)
+# 2. != (not equal)
+# 3. > (greater than)
+# 4. < (less than)
+# 5. >= (greater than or equal to)
+# 6. <= (less than or equal)
