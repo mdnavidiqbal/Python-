d52c53b6b3ff6a1f4248c91d507375b290d2547d
Md. Navid Iqbal <144615765+mdnavidiqbal@users.noreply.github.com>
2025-01-24 20:35:37 +0600

    Add files via upload

---
+++ Python_String.py
@@ -0,0 +1,47 @@
+# # Slicing String with posetve index: 
+# # Syntex: [Start:Stop:Step]
+# x = "Navid" # Index: 0 1 2 3 4
+# print(x[0:4:1]) # Ans: Navi
+# print(x[0:]) # Navid  
+
+# y = "NavidIqbal" # Index: 0 1 2 3 4 5 6 7 8 9
+# print(y[5:10:1]) # Ans : Iqbal 
+# print(y[5:]) # Iqbal 
+
+# # Slicing String with negative index: 
+# # reverse the string: 
+# y = "NavidIqbal"
+# print(y[-1:-11:-1]) # Ans :labIdivaN
+
+
+# # String Modify 
+# # upper() method use to return the string with uppper case :
+# My_String="Navid Iqbal"
+# print(My_String)
+# print(My_String.upper())
+
+# # lower() method use to return the string with lower case : 
+# n = "NAVID IQBAL"
+# print(n)
+# print(n.lower())
+
+# # strip() method use to remove the whitespace from the beginning or the end :
+# a = " Hello World "
+# print(a)
+# print(a.strip())
+
+# # replace() method use to replaces a string with another string 
+# b = "Lavid,Iqbal"
+# print(b)
+# print(b.replace("L","N"))
+
+# # String Formating 
+# num1= 5
+# num2= 6
+# sum = num1 + num2
+# print(sum)
+# # String formatting 
+# print(f"{num1} + {num2} = {num1 + num2}")
+# # print space or break in line 
+# print("Navid Iqbal.",end="")
+# print(1904538259)
