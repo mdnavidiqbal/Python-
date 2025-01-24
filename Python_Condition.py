a19d27e4650a5d569381989ccc4f9e3b7cb6be4c
Md. Navid Iqbal <144615765+mdnavidiqbal@users.noreply.github.com>
2025-01-24 19:47:14 +0600

    Update Python_Condition.py

--- Python_Condition.py
+++ Python_Condition.py
@@ -1,36 +1,42 @@
-""""
-# Syntex of if statement, 
- if logical_expression:
- print("Something");
-else: 
-print("Others ");
-"""
-"""
-number = int(input("Enter any number : "));
-if number % 2 == 0: 
-    print("ODD")
-else:
-    print("EVEN")
-"""
-"""
-# elif statement 
-if logicial_statement:
-print("Something")
-elif another_statement:
-print("Others");
-else:
-print("Last thngs")
-"""
-"""
-number=int(input("Enter the marks : "));
-if number>=80 and number<=100:
-    print("A")
-elif number>=60 and number<=80:
-    print("B")
-elif number>=50 and number<=60:
-    print("C")
-elif number>=40 and number<=50:
-    print("D")
-else:
-    print("Fail")
-"""
+# # Python_Condition and If Statement: # #
+
+# # Equals : a==b
+# # Not Equals : a != b
+# # Less than : a < b 
+# # Less than or equal to :  a <= b
+# # Grater than : a > b
+# # Grater than or equal to : a >= b 
+
+
+# # Syntex of if statement, 
+#  if logical_expression:
+#  print("Something");
+# else: 
+# print("Others ");
+
+# number = int(input("Enter any number : "));
+# if number % 2 == 0: 
+#     print("ODD")
+# else:
+#     print("EVEN")
+
+# # elif statement 
+# if logicial_statement:
+# print("Something")
+# elif another_statement:
+# print("Others");
+# else:
+# print("Last thngs")
+
+# number=int(input("Enter the marks : "));
+# if number>=80 and number<=100:
+#     print("A")
+# elif number>=60 and number<=80:
+#     print("B")
+# elif number>=50 and number<=60:
+#     print("C")
+# elif number>=40 and number<=50:
+#     print("D")
+# else:
+#     print("Fail")
+
