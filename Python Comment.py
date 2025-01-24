162afd399698d3681833e235fb4a3a1418eea93c
Md. Navid Iqbal <144615765+mdnavidiqbal@users.noreply.github.com>
2025-01-24 19:51:47 +0600

    Add files via upload

---
+++ Python Comment.py
@@ -0,0 +1,5 @@
+# Comment amra use kori code er kuno ekta specific jayga ba function ki jonno use korchii oita short e likhe rakha  
+# print() function eta amra use kori muloto kuno jinish er output janar jonno 
+# Multi line comment er jonno """ """ eita use kori 
+
+
---
+++ Python Loop.py
@@ -0,0 +1,93 @@
+
+# # while loop 
+# syntex of while loop 
+# while condition:
+# print("Problem")
+# count +=1
+
+"""
+count = 0
+while count<10: 
+    print("I love you ")
+    count +=1
+"""
+"""
+# for loop 
+Syntex of for loop : 
+for i in(1,3):
+print("Something")
+"""
+"""
+# while loop 
+num =  0;
+while num < 10 : 
+    print("Navid",num)
+    num += 1 
+# for loop
+for nam in(range(1,10)):
+        print(nam)
+
+# Print sum of all even numbers from 2 to 22
+sum = 0
+for i in range(2, 22, 2):
+    sum = sum + i
+print(sum)
+"""
+
+# food_list = ["Apple","Orange","Bannana","Jackfruit","Lichi"]
+# for i in food_list:
+#     # if i == "Orange":
+#     #     break
+#  print(i)
+
+## For Loop Problem and solution : 
+
+# # Problem-1: Print first 10 numbers using a for loop :
+# for num in range(10):
+#     print(num )
+
+# # Problem-2: Print sum of all even numbers from 2 to 22 : 
+# sum = 0 
+# for num in range(2,22,2):
+#     sum = sum + num 
+# print(sum)
+
+# # Problem-3: Calculate the square of each number of list: 
+# numbers = [1,2,3,4,5]
+# for num in numbers:
+#     print(num,"Square is: ",num**2)
+
+# #Problem-3: Calculate the average of the list number : 
+# number = [10,20,30,40,50]
+# sum = 0
+# for num in number:
+#     sum = sum + num 
+# number_len = len(number)
+# average = sum / number_len
+# print(average)
+
+# # Problem-4: Print all even and odd numbers 0 to 20 : 
+# for num in range(0,20):
+#     if num % 2 == 0 : 
+#         print("Evenr number is : ",num)
+#     else: 
+#         print("Odd number is : ",num)
+
+# # Problem-5: Brek the loop if number a number is grater than 15 : 
+# numbers = [1,4,7,8,15,20,35,45,55]
+# for num in numbers:
+#     if num > 15:
+#         break
+#     else:
+#      print(num)
+
+# # # Problem-7: Reversed the list usig for loop :  
+# number = [10,20,30,40,50]
+# for num in reversed(number):
+#     print(num) 
+
+# # Problem-8: Reversed the fixe number : 
+# num = 5
+# for num in range(num,-1,-1):
+#  print(num)
+    
